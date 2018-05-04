bd8e52f157162c9974db4dc3e1955eb6ccfc4873
Karl Amoguis <ktamoguis@gmail.com>
2018-05-04 13:32:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,9 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    song = self.new 
-    song.title = filename.split(" - ")[1]
+    song = self.new
+    song.name = filename.split(" - ")[1]
+    song.artist = filename.split(" - ")[0]
     song
   end
 
