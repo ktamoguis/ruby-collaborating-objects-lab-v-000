53382932235c7989649de7a8bfe333190731eea1
Karl Amoguis <ktamoguis@gmail.com>
2018-05-04 15:57:52 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -14,7 +14,7 @@ class MP3Importer
 
   def files
     #Dir.chdir(@filepath)
-    @files = Dir.glob("#{@filepath}/*.mp3").collect { |file| file.gsub("#{@filepath}/mp3s/","")}
+    @files = Dir.glob("#{@filepath}/*.mp3").collect { |file| file.gsub("#{@filepath}","")}
 
     binding.pry
     @files
