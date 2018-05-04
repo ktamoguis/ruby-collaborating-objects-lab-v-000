cc81163601578ab758406b3c400cad1bb2258bfb
Karl Amoguis <ktamoguis@gmail.com>
2018-05-04 13:48:47 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,5 +1,13 @@
 class MP3Importer
+
+  attr_accessor :filepath
+
+  def initialize(filepath)
+    @filepath = filepath
+  end
+  
   def import(list_of_filenames)
     list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
   end
+  
 end
